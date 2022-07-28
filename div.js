784f2161d2e7105a8fc35115ff0747f017139a54
DanVR <daniel.vinues@gmail.com>
2022-07-28 13:58:34 +0200

    Add div.js index.html

--- div.js
+++ div.js
@@ -3,12 +3,22 @@ const container = document.querySelector('#container');
 const content = document.createElement('div');
 content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
-
+//p
+const p1 = document.createElement('p');
+p1.textContent = 'Hey Im red!';
+p1.style.cssText = 'red';
+// h3
+const h3_1 = document.createElement('h3');
+h3_1.textContent = 'Im blue h3!';
+h3_1.style.cssText = 'blue';
+// div
+const div1 = document.createElement('div');
+div1.setAttribute('style', 'background: pink;border:1px solid black;');
+div1.classList.add('content'); 
+var h1_1 = document.createElement("div"); 
+div1.appendChild(h1_1);
+var p2 = document.createElement("p");
+div1.appendChild(p2);
+h1_1.textContent = 'Im a div!';
+p2.textContent = 'ME TOO!';
 container.appendChild(content);
-
-//var p1 = container.createElement("p");
-//var text = container.createTextNode("Hey Im red!");
-//p1.appendChild(text); 
-//var element1 = container.getElementsByTagName("body")[0];
-//element1.appendChild(p1);
-//container.style.cssText = 'color: red';
