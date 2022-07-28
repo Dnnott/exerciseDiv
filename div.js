c50b6a861cbc9791e4c0eed944fe24ad959d3d1d
DanVR <daniel.vinues@gmail.com>
2022-07-28 14:27:29 +0200

    Add div.js

--- div.js
+++ div.js
@@ -5,27 +5,27 @@ content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
+
 //p
-content.classList.add('p1');
 const p1 = document.createElement('p');
+content.appendChild(p2);
 p1.textContent = 'Hey Im red!';
 p1.style.cssText = 'red';
 // h3
-content.classList.add('h3_1');
 const h3_1 = document.createElement('h3');
 h3_1.textContent = 'Im blue h3!';
 h3_1.style.cssText = 'blue';
+content.appendChild(h3_1);
 // div
-content.classList.add('div1');
 const div1 = document.createElement('div');
 div1.setAttribute('style', 'background: pink;border:1px solid black;');
 div1.classList.add('content'); 
+content.appendChild(div1);
     //h1
-    div1.classList.add('h1_1');
+
     var h1_1 = document.createElement("div"); 
     div1.appendChild(h1_1);
     //p
-    div1.classList.add('p2');
     var p2 = document.createElement("p");
     div1.appendChild(p2);
     h1_1.textContent = 'Im a div!';
