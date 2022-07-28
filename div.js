b9a76efd3e80606a9f50d0d93b7aadcebc356d36
DanVR <daniel.vinues@gmail.com>
2022-07-28 17:52:09 +0200

    Add div.js

--- div.js
+++ div.js
@@ -9,7 +9,7 @@ container.appendChild(content);
 
 //p1
 const p1 = document.createElement('p');
-p1.classList.add('p');
+p1.classList.add('p1');
 p1.textContent = 'Hey Im red!';
 p1.style.cssText = 'red';
 content.appendChild(p1);
@@ -19,18 +19,24 @@ const h3_1 = document.createElement('h3');
 h3_1.textContent = 'Im blue h3!';
 h3_1.style.cssText = 'blue';
 content.appendChild(h3_1);
+
 // div
 const div1 = document.createElement('div');
-div1.setAttribute('style', 'background: pink;border:1px solid black;');
-div1.classList.add('content'); 
+div1.setAttribute('background: pink;border:1px solid black;');
+div1.classList.add('div1'); 
 content.appendChild(div1);
     //h1
-
-    var h1_1 = document.createElement("div"); 
+    const h1_1 = document.createElement('h1');
+    h1_1.classList.add('h1_1');
+    h1_1.textContent = 'Im a div!';
     div1.appendChild(h1_1);
+    
     //p
-    var p2 = document.createElement("p");
+    const p2 = document.createElement('p');
+    p2.classList.add('p2');
+    p2.textContent = 'ME TOO!';
     div1.appendChild(p2);
+
     h1_1.textContent = 'Im a div!';
     p2.textContent = 'ME TOO!';
 
