66143874bbf47234f68ef2938707da1446c2c17e
DanVR <daniel.vinues@gmail.com>
2022-07-28 19:46:14 +0200

    Add div.js

--- div.js
+++ div.js
@@ -23,7 +23,7 @@ content.appendChild(h3_1);
 
 // div
 const div1 = document.createElement('div');
-div1.setAttribute('style', 'color: blue; background: pink; border-color: black; border-width: 1px'); 
+div1.setAttribute('style', 'color: blue; background: pink; borderColor: black; borderWidth: 1px'); 
 div1.classList.add('div1'); 
 content.appendChild(div1);
 
